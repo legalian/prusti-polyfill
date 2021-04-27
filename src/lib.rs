ccec4df411d8d6a7f510235f2f39fcb67334872b
Parker Lawrence <liverspleenkidney@gmail.com>
2021-04-27 14:42:32 -0400

    support for other prusti macros

--- src/lib.rs
+++ src/lib.rs
@@ -16,12 +16,31 @@ return "
 }
 
 #[proc_macro_attribute]
-pub fn trusted(_attr: TokenStream, item: TokenStream) -> TokenStream {
-	println!("trusting!");
-	return item;
+pub fn requires(_attr: TokenStream, item: TokenStream) -> TokenStream {item}
+#[proc_macro_attribute]
+pub fn ensures(_attr: TokenStream, item: TokenStream) -> TokenStream {item}
+#[proc_macro_attribute]
+pub fn after_expiry(_attr: TokenStream, item: TokenStream) -> TokenStream {item}
+#[proc_macro_attribute]
+pub fn after_expiry_if(_attr: TokenStream, item: TokenStream) -> TokenStream {item}
+#[proc_macro_attribute]
+pub fn pure(_attr: TokenStream, item: TokenStream) -> TokenStream {item}
+#[proc_macro_attribute]
+pub fn trusted(_attr: TokenStream, item: TokenStream) -> TokenStream {item}
+#[proc_macro]
+pub fn body_invariant(_item: TokenStream) -> TokenStream {return "".parse().unwrap();}
+macro_rules! closure {
+    ($($b:tt)*,$a:expr)=>{$a}
 }
+#[proc_macro_attribute]
+pub fn refine_trait_spec(_attr: TokenStream, item: TokenStream) -> TokenStream {item}
+#[proc_macro_attribute]
+pub fn extern_spec(_attr: TokenStream, _item: TokenStream) -> TokenStream {return "".parse().unwrap();}
+#[proc_macro_attribute]
+pub fn predicate(_attr: TokenStream, _item: TokenStream) -> TokenStream {return "".parse().unwrap();}
 
-
-
-
+#[proc_macro_attribute]
+pub fn refine_requires(_attr: TokenStream, item: TokenStream) -> TokenStream {item}
+#[proc_macro_attribute]
+pub fn refine_ensures(_attr: TokenStream, item: TokenStream) -> TokenStream {item}
 
