97eba331df76c607e2103565d363f8938bb3e5de
Parker Lawrence <liverspleenkidney@gmail.com>
2021-04-27 11:53:51 -0400

    cfg

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,9 @@ use proc_macro::TokenStream;
 
 #[proc_macro]
 pub fn prusti_polyfill_init(_item: TokenStream) -> TokenStream {
-	#[cfg(feature = "verified")]
-    return "extern crate proc_macro;use proc_macro::TokenStream;".parse().unwrap();
-    #[cfg(not(feature = "verified"))]
-    return "".parse().unwrap();
+    return "#[cfg(feature = \"verified\")]\nextern crate proc_macro;\n#[cfg(feature = \"verified\")]\nuse proc_macro::TokenStream;".parse().unwrap();
 }
 
-#[cfg(not(feature = "verified"))]
 #[proc_macro_attribute]
 pub fn trusted(_attr: TokenStream, item: TokenStream) -> TokenStream {
 	println!("trusting!");
