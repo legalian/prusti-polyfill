0002edd68192fc6c75a0d9fc53406b538e5a87d3
Parker Lawrence <liverspleenkidney@gmail.com>
2021-04-27 12:53:05 -0400

    get rid of ambiguity

--- src/lib.rs
+++ src/lib.rs
@@ -5,13 +5,18 @@ use proc_macro::TokenStream;
 
 #[proc_macro]
 pub fn prusti_polyfill_init(_item: TokenStream) -> TokenStream {
-    return "#[cfg(feature = \"verified\")]\nextern crate prusti_contracts;\n#[cfg(feature = \"verified\")]\nuse prusti_contracts::*;".parse().unwrap();
+return "
+	#[cfg(feature = \"verified\")]
+	extern crate prusti_contracts;
+	#[cfg(feature = \"verified\")]
+	use prusti_contracts::trusted;
+".parse().unwrap();
 }
 
 #[proc_macro_attribute]
 pub fn trusted(_attr: TokenStream, item: TokenStream) -> TokenStream {
 	println!("trusting!");
-    return item;
+	return item;
 }
 
 
